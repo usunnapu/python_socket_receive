bbe98d6c46907584901bb5cf1ddf61fd8a10607e
usunnapu <40170140+usunnapu@users.noreply.github.com>
2020-05-16 14:28:55 -0500

    Update receive.py

--- receive.py
+++ receive.py
@@ -1,9 +1,23 @@
-import socket
+import socket, sys
 
-def receive():
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(("", 4000))
-        s.listen(2)
-        conn, addr = s.accept()
-        print("accepted")
-        print(bytes.decode(conn.recv(1024))
+socket.setdefaulttimeout(150)
+host = ''
+port = 50005
+socksize = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+print("Server started on port: {}".format(port))
+s.listen(1)
+conn, addr = s.accept()
+print('New connection from {}:{}'.format(addr[0], addr[1]))
+print("Now listening...\n")
+while True:
+    data = conn.recv(socksize)
+    if not data:
+        break
+    elif data == 'killsrv':
+        conn.close()
+        sys.exit()
+    else:
+       print(data)
